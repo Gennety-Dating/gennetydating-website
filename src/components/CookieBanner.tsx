ee176504c9dface13dd59eecf0af66677a03ef91
Gleb4442 <217622719+Gleb4442@users.noreply.github.com>
2026-05-28 16:45:22 +0300

    Change Spotify widget height to 80px for compact view

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -192,7 +192,7 @@ function SpotifyWidget() {
         style={{ borderRadius: 12 }}
         src="https://open.spotify.com/embed/track/7BKLCZ1jbUBVqRi2FVlTVw?utm_source=generator"
         width="100%"
-        height="152"
+        height="80"
         frameBorder="0"
         allowFullScreen
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
